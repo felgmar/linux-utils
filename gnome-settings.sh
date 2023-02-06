482906763578fcfa6f083488a2c5b4a598b7f950
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2023-02-06 10:38:24 +0000

    Minor tweaks

--- gnome-settings.sh
+++ gnome-settings.sh
@@ -2,14 +2,29 @@
 
 apply()
 {
-    echo "Applying settings..."
+    echo "Setting 'clock-show-seconds' to 'true'..."
     gsettings set org.gnome.desktop.interface clock-show-seconds true || return 1
+    echo "Setting 'clock-show-date' to 'true'..."
     gsettings set org.gnome.desktop.interface clock-show-date true || return 1
+    echo "Setting 'show-weekdate' to 'true'..."
     gsettings set org.gnome.desktop.calendar show-weekdate true || return 1
-    gsettings set org.gnome.desktop.interface icon-theme Papirus || return 1
+
+    if test ! -z `pacman -Qeq | grep papirus-icon-theme`
+    then
+        echo "Setting 'icon-theme' to 'Papirus'..."
+        gsettings set org.gnome.desktop.interface icon-theme Papirus || return 1
+    else
+        echo "[E]: Could not find the icon-theme named 'Papirus'"
+    fi
+    echo "Setting 'button-layout' to 'appmenu:minimize,maximize,close'..."
     gsettings set org.gnome.desktop.wm.preferences button-layout appmenu:minimize,maximize,close || return 1
 
-    test $? && echo "success" || echo "failure"
+    if test $? -eq 0
+    then
+        echo "success"
+    else
+        echo "failure"
+    fi
 
     return $?
 }
