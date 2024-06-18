fd02d344f4b6db4dd9d8424824cbc1286f48cba6
Felipe González Martín <158048821+mrkenhoo@users.noreply.github.com>
2024-06-18 15:55:15 +0100

    add new utility for setting up the firewall

---
+++ setup-firewalld.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+DEFAULT_ZONE="$(firewall-cmd --get-default-zone)"
+SERVICES=("kdeconnect")
+SOURCES=("192.168.1.0/28")
+
+set_default_zone_block()
+{
+    if test "${DEFAULT_ZONE}" = "public"
+    then
+        sudo firewall-cmd --set-default-zone block
+    else
+        echo "[warn] The firewalld default zone is already set to: ${DEFAULT_ZONE}"
+        return 2
+    fi
+
+    return $?
+}
+
+add_services()
+{
+    for service in ${SERVICES[@]}
+    do
+        if test ! "$(sudo firewall-cmd --list-service | grep -o ${service})" = "${service}"
+        then
+            sudo firewall-cmd --permanent --add-service "${service}"
+        else
+            echo "[error] ${service}: service already enabled"
+        fi
+    done
+
+    if test $? -ne 0
+    then
+        sudo firewall-cmd --complete-reload
+    fi
+
+    return $?
+}
+
+add_sources()
+{
+    for source in ${SOURCES[@]}
+    do
+        if test ! "$(sudo firewall-cmd --list-service | grep -o ${source})" = "${source}"
+        then
+            sudo firewall-cmd --permanent --add-sources "${source}"
+        else
+            echo "[error] ${source}: source already enabled"
+        fi
+    done
+
+    if test $? -ne 1
+    then
+        sudo firewall-cmd --complete-reload
+    fi
+
+    return $?
+}
+
+set_default_zone_block
+add_services
+
