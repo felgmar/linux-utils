673cb141f2962bbb2886664b1ff546fd3486c931
Felipe González Martín <158048821+felgmar@users.noreply.github.com>
2025-08-14 12:26:41 +0100

    update-mirrors: minor code rewriting for better readability

--- update-mirrors.py
+++ update-mirrors.py
@@ -1,32 +1,53 @@
 #!/usr/bin/env python3
 
-from argparse import ArgumentParser
-from getpass import getuser
-from sys import platform
-from subprocess import run
-from os import path
-
-parser = ArgumentParser(prog="update-mirrors")
-parser.add_argument("-d", "--distro", type=str, metavar="", default="arch",
-                    help="the distro for which to download mirrors for")
-parser.add_argument("-s", "--save", type=str, metavar="", default="/etc/pacman.d/mirrorlist",
-                    help="directory where the mirror list will be saved to")
-parser.add_argument("--version", action="version", version="%(prog)s 1.0")
-
-args = parser.parse_args()
-
 if __name__ == "__main__":
+    from argparse import ArgumentParser
+    from getpass import getuser
+    from sys import platform
+    from subprocess import run
+    from os import path
+
+    VALID_DISTROS: list[str] = [ "arch" ]
+    SAVE_DIRECTORY: list[str] = [ "/etc/pacman.d/mirrorlist" ]
     CURRENT_PLATFORM: str = platform.lower()
     CURRENT_USER: str = getuser()
 
     if not CURRENT_PLATFORM == "linux":
         raise RuntimeError(f"{CURRENT_PLATFORM} is not supported")
-    
+
     if not CURRENT_USER == "root":
         raise PermissionError("you don't have enough permissions to run this script")
+
+    parser = ArgumentParser(prog="update-mirrors",
+                            description="Update mirror lists for Linux distributions")
+    parser.add_argument("-d", "--distro",
+                        type=str,
+                        metavar="",
+                        default="arch",
+                        choices=VALID_DISTROS,
+                        help="the distribution for which to setup mirrors" +
+                        f" (supported: {', '.join(VALID_DISTROS)})")
+    parser.add_argument("-s", "--save",
+                        type=str,
+                        metavar="",
+                        default="/etc/pacman.d/mirrorlist",
+                        choices=SAVE_DIRECTORY,
+                        help="directory where the mirror list will be saved to" +
+                        f" (supported: {', '.join(SAVE_DIRECTORY)})")
+    parser.add_argument("--version",
+                        action="version",
+                        version="%(prog)s 1.0")
+    args = parser.parse_args()
+
+    if not args.save in SAVE_DIRECTORY:
+        raise FileNotFoundError(f"{args.save} is not a valid save directory")
 
     if not path.isfile(args.save):
         raise FileNotFoundError(f"{args.save} does not exist")
 
-    cmd: str = f"rate-mirrors --allow-root --save {args.save} {args.distro}"
-    run(args=cmd, shell=True)
+    CMD: str = f"rate-mirrors --allow-root --save {args.save} {args.distro}"
+
+    try:
+        run(args=CMD, shell=True)
+    except Exception as e:
+        raise e
